aa893311e5cea45cd2590fd4dc53d55794f1acc3
coderpr0grammer <daniellhf@icloud.com>
2023-04-17 16:42:38 -0400

    popup

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -2,24 +2,16 @@ import React from 'react';
 import logo from '../../assets/img/logo.svg';
 import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
+import { Button } from 'react-bootstrap';
+import image from '../../assets/img/icon-128.png'
 
 const Popup = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
+    <div className="App" style={{ flex: 1, width: '100%', height: '100%' }}>
+      <h1 style={{ lineHeight: 1, marginTop: 10, marginBottom: 0 }}>Ctrl+F on steroids</h1>
+      <h4 style={{ marginTop: 5 }}>powered by ai 🤖</h4>
+      <img src={image} width="300px"></img>
+      <Button variant="primary">Helo</Button>
     </div>
   );
 };
