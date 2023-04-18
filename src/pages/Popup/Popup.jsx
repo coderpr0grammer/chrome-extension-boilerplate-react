3b09b4bc56bd7ef33a6b4fb20bd0ffa8152014cf
Daniel Martinez <daniellhf@icloud.com>
2023-04-18 08:06:13 -0400

    ok

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -3,14 +3,13 @@ import logo from '../../assets/img/logo.svg';
 import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
 import { Button } from 'react-bootstrap';
-import image from '../../assets/img/icon-128.png'
+import image from '../../assets/img/icon-128.png';
 
 const Popup = () => {
   return (
     <div className="App" style={{ flex: 1, width: '100%', height: '100%' }}>
-      <h1 style={{ lineHeight: 1, marginTop: 10, marginBottom: 0 }}>Ctrl+F on steroids</h1>
+      <h1 style={{ lineHeight: 1, marginTop: 10, marginBottom: 0 }}>Skym 🤖</h1>
       <h4 style={{ marginTop: 5 }}>powered by ai 🤖</h4>
-      <img src={image} width="300px"></img>
       <Button variant="primary">Helo</Button>
     </div>
   );
