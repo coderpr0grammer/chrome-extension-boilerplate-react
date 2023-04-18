60e85cea9faac38808d0b744bad292b3924c1ce2
coderpr0grammer <daniellhf@icloud.com>
2023-04-18 16:46:56 -0400

    popup styling

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -7,7 +7,10 @@ import image from '../../assets/img/icon-128.png';
 
 const Popup = () => {
   return (
-    <div className="App" style={{ width: 800, height: 800 }}>
+    <div className="App" style={{
+      background: 'black', borderWidth: 10, border: 'solid 10px',
+      borderImage: 'linear-gradient(to bottom right, #FF914D 0%, #F67B30 50%, #EF2E2E 100%) 1',
+    }}>
       <h1 style={{ lineHeight: 1, marginTop: 10, marginBottom: 0, position: 'fixed' }}>Skm.ai 🤖</h1>
 
     </div>
