116c5f627655baaeada0ef196014b912ca6a7681
coderpr0grammer <daniellhf@icloud.com>
2023-04-28 10:41:38 -0400

    Update index.jsx

--- src/pages/Content/index.jsx
+++ src/pages/Content/index.jsx
@@ -17,8 +17,29 @@ if (isProbablyReaderable(document)) {
     );
     var documentClone = document.cloneNode(true);
     var article = new Readability(documentClone).parse();
-    console.log(article.textContent)
+    // console.log(article.textContent)
     console.log('is readable')
+    // Define the text to search for
+    const searchText = "Bacteria are ubiquitous";
+
+    // Get all the elements on the page
+    const elements = document.querySelectorAll("*");
+
+    // Loop through each element and check if it contains the search text
+    let searchQuery = "Bacteria are ubiquitous";
+    let textNodes = document.querySelectorAll("body *");
+
+    for (let i = 0; i < textNodes.length; i++) {
+        let node = textNodes[i];
+        if (node.nodeType === Node.TEXT_NODE) {
+            let searchText = node.textContent;
+            if (searchText.indexOf(searchQuery) !== -1) {
+                // Do something with the text node that contains the search query
+                // node.style.backgroundColor = "yellow";
+                console.log(node)
+            }
+        }
+    }
 } else {
     console.log('not readable')
 
