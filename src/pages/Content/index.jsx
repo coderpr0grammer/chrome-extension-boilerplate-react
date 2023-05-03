be2897154363a76125f9daa6c9646a74bca4378d
Daniel Martinez <daniellhf@icloud.com>
2023-05-02 22:12:34 -0400

    Update index.jsx

--- src/pages/Content/index.jsx
+++ src/pages/Content/index.jsx
@@ -1,31 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Popup from './Popup'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Popup from './Popup';
 var { Readability, isProbablyReaderable } = require('@mozilla/readability');
-import Mark from "mark.js";
+// import Mark from 'mark.js';
 
-const rootElement = document.createElement("div");
-rootElement.id = "react-chrome-app";
+const rootElement = document.createElement('div');
+rootElement.id = 'react-chrome-app';
 
 document.body.appendChild(rootElement);
 
 if (isProbablyReaderable(document)) {
-    const root = ReactDOM.createRoot(rootElement);
-    root.render(
-        <React.StrictMode>
-            <Popup />
-        </React.StrictMode>
-    );
-    var documentClone = document.cloneNode(true);
-    var article = new Readability(documentClone).parse();
-    console.log(article.textContent)
-    console.log('is readable')
-    var instance = new Mark(document);
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <Popup />
+    </React.StrictMode>
+  );
+  var documentClone = document.cloneNode(true);
+  var article = new Readability(documentClone).parse();
+  console.log(article);
+  let segments = article.textContent.split('\n');
+  segments.forEach((item, index) => {
+    console.log('length: ', item.length);
+  });
+  // console.log(article.content.split('\n'));
+  console.log('is readable');
+  // var instance = new Mark(document);
 
-    // instance.mark("TA43 was still in commission and available to fight, although she saw little action.[27]", { "accuracy": "complementary", "diacritics": true });
-
-
+  // instance.mark("TA43 was still in commission and available to fight, although she saw little action.[27]", { "accuracy": "complementary", "diacritics": true });
 } else {
-    console.log('not readable')
-
+  console.log('not readable');
 }
