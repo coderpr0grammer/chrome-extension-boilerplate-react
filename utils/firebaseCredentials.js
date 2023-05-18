b45b62f21beecd4fa477d8224f15392d49b2a1ed
Daniel Martinez <daniellhf@icloud.com>
2023-05-18 16:31:42 -0400

    remove analytics

--- utils/firebaseCredentials.js
+++ utils/firebaseCredentials.js
@@ -1,23 +1,23 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+// import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCWRh1dorXANIxmMIHoUTcOAjcDmj5dOR8",
-    authDomain: "skm-extension-official.firebaseapp.com",
-    projectId: "skm-extension-official",
-    storageBucket: "skm-extension-official.appspot.com",
-    messagingSenderId: "173016892459",
-    appId: "1:173016892459:web:a7bfb3456c526c2a8a966e",
-    measurementId: "G-6VJSX11TV9"
+  apiKey: 'AIzaSyCWRh1dorXANIxmMIHoUTcOAjcDmj5dOR8',
+  authDomain: 'skm-extension-official.firebaseapp.com',
+  projectId: 'skm-extension-official',
+  storageBucket: 'skm-extension-official.appspot.com',
+  messagingSenderId: '173016892459',
+  appId: '1:173016892459:web:a7bfb3456c526c2a8a966e',
+  measurementId: 'G-6VJSX11TV9',
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
